202bea92c4f211dcf29539ae8512248448b80f87
SebastianVillegasVelasquez <villegasvelasquezs@gmail.com>
2023-11-21 12:02:35 -0500

    first commit

---
+++ src/main/java/com/example/proyecto_universidad/ProyectoUniversidadApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_universidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUniversidadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoUniversidadApplication.class, args);
+    }
+
+}
